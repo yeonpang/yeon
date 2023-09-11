6e7d7677c6454ab23ab881c910039a4a4ca6751a
yeonpang <143787849+yeonpang@users.noreply.github.com>
2023-09-11 17:53:21 +0900

    Add files via upload

---
+++ m.py
@@ -0,0 +1,251 @@
+"""my_set = {5, 8, 3, 7, 1, "h"}
+
+print(my_set)
+
+set_tmp = set("hello")
+
+print(set_tmp)
+"""
+
+"""my_set = {5, 8, 3, 7, 1, "h"}
+set_item = {7, 8, 4, 2, "h"}
+
+print(my_set | set_item)
+"""
+
+"""my_set = {5, 8, 3, 7, 1, "h"}
+set_item = {7, 8, 4, 2, "h"}
+
+#print(my_set | set_item)
+
+print(my_set.union(set_item))
+"""
+
+"""my_set = {5, 8, 3, 7, 1, "h"}
+set_item = {7, 8, 4, 2, "h"}
+
+print(my_set | set_item)
+
+print(my_set - set_item)
+"""
+
+"""my_set = {5, 8, 3, 7, 1, "h"}
+set_item = {7, 8, 4, 2, "h"}
+
+print(my_set | set_item)
+
+#print(my_set - set_item)
+
+print(my_set.difference(set_item))
+
+my_set = {5, 8, 3, 7, 1, "h"}
+set_item = {7, 8, 4, 2, "h"}
+
+print(my_set | set_item)
+
+print(my_set & set_item)
+"""
+"""my_set = {5, 8, 3, 7, 1, "h"}
+set_item = {7, 8, 4, 2, "h"}
+
+print(my_set | set_item)
+
+#print(my_set & set_item)
+
+print(my_set.intersection(set_item))
+"""
+"""my_set = {5, 8, 3, 7, 1, "h"}
+
+print(my_set)
+
+my_set.add(9)
+
+print(my_set)
+
+my_set = {5, 8, 3, 7, 1, "h"}
+
+print(my_set)
+
+my_set.update([5, 9, 7, 4])
+
+print(my_set)
+"""
+"""my_set = {5, 8, 3, 7, 1, "h"}
+
+print(my_set)
+
+my_set.clear()
+"""
+"""my_set = {5, 8, 3, 7, 1, "h"}
+
+print(my_set)
+
+my_set.remove(5)
+
+my_set = {5, 8, 3, 7, 1, "h"}
+
+print(my_set)
+
+my_set.discard(2)
+print(my_set)
+"""
+
+"""my_set = {5, 8, 3, 7, 1, "h"}
+set_item = {7, 8, 4, 2, "h"}
+
+print(my_set)
+
+my_set.difference_update(set_item)
+print(my_set)
+"""
+
+"""my_int = 10
+print(my_int)
+
+print(my_int + 10)
+
+my_int = 10
+print(my_int)
+
+print(my_int + 10)
+my_str = str(my_int)
+
+print(my_str)
+"""
+"""my_int = 10
+print(my_int)
+
+print(my_int + 10)
+my_str = str(my_int)
+
+print(my_str)
+
+print(my_str + 10)
+"""
+
+"""my_int = 10
+print(my_int)
+
+print(my_int + 10)
+my_str = str(my_int)
+
+print(my_str)
+
+print(my_str + " hello ")
+"""
+
+"""my_str = '10'
+print(my_str)
+
+my_int = int(my_str)
+print(my_int)
+"""
+"""my_str = '10'
+print(my_str)
+
+my_int = int(my_str)
+print(my_int)
+
+print(my_int + 10)
+"""
+
+"""my_str = '10'
+print(my_str)
+
+my_int = int(my_str)
+print(my_int)
+
+print(my_int + 10)
+
+my_int2 = int("ten")
+print(my_int2)
+"""
+
+"""a = 10
+b = 3
+
+# c = a + b
+# c = a - b
+# c = a / b
+# c = a * b
+# c = a % b
+# c = a // b
+c = a ** b
+
+print(c)
+"""
+
+"""a = 0
+print(a)
+
+a += 2
+print(a)
+
+a -= 1
+print(a)
+
+a *= 4
+print(a)
+
+a /= 2
+print(a)
+
+a **= 3
+print(a)
+"""
+
+"""a = 10
+b = 4
+
+# c = a > b
+# c = a < b
+# c = a >= b
+# c = a <= b
+# c = a == b
+c = (a != b)
+
+print(c)
+"""
+
+"""a = 1
+b = 0
+
+print(a and b)
+print(a or b)
+print(not b)
+
+
+
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
+print(not y)
+"""
+
+"""a = 10
+b = 3
+
+# c = a & b
+# c = a | b
+# c = a ^ b
+# c = ~ a
+c = a << 2
+print(c)
+ """
+
+"""my_list = [9, 4, 3, 7, 8, 'hi']
+
+print(2 not in my_list)
+
+my_dic = {"key1" : "v1", "k2" : "v2"}
+print("k" in my_dic)
+
+# x = [1, 2, 4]
+# y = [1, 2, 3]
+# z = x
+
+# print(x is y)
+"""
